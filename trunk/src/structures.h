a0c0470a307f1b4d4b6bbad4514d230c23ae5d9c
agaldor <agaldor@af3b5ab6-e54d-0410-8989-4f5c6538ae25>
2008-05-23 14:05:12 +0000

    Douple pasted license text.

--- trunk/src/structures.h
+++ trunk/src/structures.h
@@ -5,13 +5,6 @@ flexNES - Flexible NAT Emulation Software
 Copyright (C) 2008, Jeremy Beker <gothmog@confusticate.com>
 
 This program is free software; you can redistribute it and/or
-/*
-
-flexNES - Flexible NAT Emulation Software
-
-Copyright (C) 2008, Jeremy Beker <gothmog@confusticate.com>
-
-This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
